e68f96dd5e1a3142003a5140f0e057dc218be036
Tomek Kleszcz <tomekkleszcz@icloud.com>
2023-07-07 12:28:26 +0200

    feat: implement support for pure a9

--- src/const/devices.ts
+++ src/const/devices.ts
@@ -1,7 +1,12 @@
 import { Comfort600 } from '../accessories/devices/comfort600';
-import { WellA7 } from '../accessories/devices/wellA7';
+import { WellA7 } from '../accessories/devices/airPurifier/wellA7';
+import { PureA9 } from '../accessories/devices/airPurifier/pureA9';
 
 export const DEVICES = {
+    /* Air conditioners */
     'Azul': Comfort600,
-    'WELLA7': WellA7
+
+    /* Air purifiers */
+    'WELLA7': WellA7,
+    'PUREA9': PureA9
 };
