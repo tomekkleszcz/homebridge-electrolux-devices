9b3eebec468c0d74ad12fb4ea39b9dd1587d1275
Mathias Fredriksson <mafredri@gmail.com>
2023-08-27 22:22:41 +0300

    feat: convert tvoc to voc density
    This commit adds support for converting TVOC to VOC Density, as tracked
    by HomeKit. This conversion does require giving the target molecular
    weight as input, though. Formeldahyde was picked as the default gas
    weight since it's a common household gas, but we let the user configure
    the gas weight just in case they have a better idea of what they want to
    track.
    
    For a TVOC of 300 ppb at 25 C and using the weight of formeldahyde, we
    get 368 μg/m^3.

--- src/accessories/devices/airPurifier/airPurifier.ts
+++ src/accessories/devices/airPurifier/airPurifier.ts
@@ -253,7 +253,13 @@ export class AirPurifier extends ElectroluxAccessoryController {
     }
 
     async getVOCDensity(): Promise<CharacteristicValue> {
-        return this.appliance.properties.reported.TVOC;
+        const vocDensity = tvocPPBToVocDensity(
+            this.appliance.properties.reported.TVOC,
+            this.appliance.properties.reported.Temp,
+            this._platform.config.vocMolecularWeight || 30.026,
+        );
+        // HomeKit VOC density is capped at 1000 μg/m^3.
+        return Math.min(vocDensity, 1000);
     }
 
     async getCurrentRelativeHumidity(): Promise<CharacteristicValue> {
@@ -350,4 +356,20 @@ export class AirPurifier extends ElectroluxAccessoryController {
             this.appliance.properties.reported.Temp,
         );
     }
+}
+
+// tvocPPBToVocDensity converts TVOC in parts per billion (ppb) to VOC density
+// (μg/m^3). This function is based on the following formula:
+//
+//	VOC density (μg/m^3) = P * MW * ppb / R * (K + T°C)
+//
+// Where:
+//   - P is the standard atmospheric pressure in kPa (1 atm = 101.325 kPa)
+//   - MW is the molecular weight of the gas in g/mol
+//   - ppb is the TVOC in parts per billion
+//   - R is the ideal gas constant
+//   - K is the standard temperature in Kelvin (0°C)
+//   - T is the provided temperature (in Celsius)
+function tvocPPBToVocDensity(ppb: number, temperature: number, molecularWeight: number) {
+    return Math.round((101.325 * molecularWeight * ppb) / (8.31446261815324 * (273.15 + temperature)));
 }
