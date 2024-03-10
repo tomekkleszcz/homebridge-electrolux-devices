b0be01a8a47300b2099bfffe09d73e3410b2d110
tomekkleszcz <tomekkleszcz@icloud.com>
2024-03-10 18:28:13 +0100

    feat(air purifier): implement support for ultimate home 500

---
+++ src/accessories/devices/airPurifier/ultimateHome500.ts
@@ -0,0 +1,88 @@
+import { CharacteristicValue, PlatformAccessory, Service } from 'homebridge';
+import { ElectroluxDevicesPlatform } from '../../../platform';
+import { AirPurifier } from './airPurifier';
+import { ElectroluxAccessoryController } from '../../controller';
+import { Appliance } from '../../../definitions/appliance';
+
+export class UltimateHome500 extends AirPurifier {
+    private uvLightService: Service;
+    private airQualityService: Service;
+
+    constructor(
+        readonly _platform: ElectroluxDevicesPlatform,
+        readonly _accessory: PlatformAccessory<ElectroluxAccessoryController>,
+        readonly _appliance: Appliance
+    ) {
+        super(_platform, _accessory, _appliance);
+
+        this.uvLightService =
+            this.accessory.getService(this.platform.Service.Lightbulb) ||
+            this.accessory.addService(this.platform.Service.Lightbulb);
+
+        this.uvLightService.setCharacteristic(
+            this.platform.Characteristic.Name,
+            'UV Light'
+        );
+
+        this.uvLightService
+            .getCharacteristic(this.platform.Characteristic.On)
+            .onGet(this.getCharacteristicValueGuard(this.getUVLight.bind(this)))
+            .onSet(
+                this.setCharacteristicValueGuard(this.setUVLight.bind(this))
+            );
+
+        this.airQualityService =
+            this.accessory.getService(this.platform.Service.AirQualitySensor) ||
+            this.accessory.addService(this.platform.Service.AirQualitySensor);
+
+        this.airQualityService
+            .getCharacteristic(this.platform.Characteristic.AirQuality)
+            .onGet(
+                this.getCharacteristicValueGuard(this.getAirQuality.bind(this))
+            );
+
+        this.airQualityService
+            .getCharacteristic(this.platform.Characteristic.PM2_5Density)
+            .onGet(
+                this.getCharacteristicValueGuard(
+                    this.getPM2_5Density.bind(this)
+                )
+            );
+    }
+
+    async getUVLight(): Promise<CharacteristicValue> {
+        return this.appliance.properties.reported.UVState === 'on';
+    }
+
+    async setUVLight(value: CharacteristicValue) {
+        await this.sendCommand({
+            UVState: value ? 'On' : 'Off'
+        });
+
+        this.appliance.properties.reported.UVState = value ? 'on' : 'off';
+    }
+
+    async getAirQuality(): Promise<CharacteristicValue> {
+        if (this.appliance.properties.reported.PM2_5_approximate <= 25) {
+            return this.platform.Characteristic.AirQuality.EXCELLENT;
+        } else if (this.appliance.properties.reported.PM2_5_approximate <= 50) {
+            return this.platform.Characteristic.AirQuality.GOOD;
+        } else if (this.appliance.properties.reported.PM2_5_approximate <= 75) {
+            return this.platform.Characteristic.AirQuality.FAIR;
+        } else if (
+            this.appliance.properties.reported.PM2_5_approximate <= 100
+        ) {
+            return this.platform.Characteristic.AirQuality.INFERIOR;
+        } else {
+            return this.platform.Characteristic.AirQuality.POOR;
+        }
+    }
+
+    async getPM2_5Density(): Promise<CharacteristicValue> {
+        return this.appliance.properties.reported.PM2_5_approximate;
+    }
+
+    async update(appliance: Appliance) {
+        super.update(appliance);
+    }
+}
--- src/const/devices.ts
+++ src/const/devices.ts
@@ -1,6 +1,7 @@
 import { Comfort600 } from '../accessories/devices/comfort600';
 import { WellA7 } from '../accessories/devices/airPurifier/wellA7';
 import { PureA9 } from '../accessories/devices/airPurifier/pureA9';
+import { UltimateHome500 } from '../accessories/devices/airPurifier/ultimateHome500';
 
 export const DEVICES = {
     /* Air conditioners */
@@ -8,5 +9,6 @@ export const DEVICES = {
 
     /* Air purifiers */
     WELLA7: WellA7,
-    PUREA9: PureA9
+    PUREA9: PureA9,
+    Muju: UltimateHome500
 };
